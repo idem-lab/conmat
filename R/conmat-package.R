01f13691850e917d3ab7f435767bbc7a98745a2f
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-09-06 23:10:14 +0800

    add package metadata doc file and pipe, fix missing comma

---
+++ R/conmat-package.R
@@ -0,0 +1,47 @@
+#' @keywords internal
+#' @importFrom stats predict
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
+globalVariables(
+  c(
+    "age_from",
+    "age_group_from",
+    "age_group_to",
+    "age_to",
+    "cnt_age",
+    "cnt_age_est_max",
+    "cnt_age_est_min",
+    "cnt_home",
+    "cnt_leisure",
+    "cnt_otherplace",
+    "cnt_school",
+    "cnt_transport",
+    "cnt_work",
+    "contacted",
+    "contacts",
+    "country",
+    "lower.age.limit",
+    "missing_any_contact_age",
+    "missing_any_contact_setting",
+    "part_age",
+    "part_id",
+    "participants",
+    "pop_age_from",
+    "population",
+    "predict",
+    "school_fraction_age_from",
+    "school_fraction_age_to",
+    "se_contacts",
+    "work_fraction_age_from",
+    "work_fraction_age_to",
+    "year"
+  )
+)
