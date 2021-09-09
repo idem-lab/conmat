b4b23daab96afe77197d38761445fdb66b62ee17
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-09-09 15:40:01 +0800

    added example of conmat use to README for Sydney

--- tests/testthat/test-check-lga-name.R
+++ tests/testthat/test-check-lga-name.R
@@ -2,9 +2,16 @@ test_that("check_lga_name() returns the right shape", {
   expect_silent(check_lga_name("Albury (C)"))
 })
 
-test_that("check_lga_name() errors", {
+test_that("check_lga_name() errors when there's an incorrect name", {
   expect_snapshot(
     error = TRUE,
     check_lga_name("Imaginary World")
+  )
+})
+
+test_that("check_lga_name() errors when the name is ambiguous", {
+  expect_snapshot(
+    error = TRUE,
+    check_lga_name("Sydney")
   )
 })
