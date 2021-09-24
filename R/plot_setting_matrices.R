88e6f6cf362627a4bb61d5d043793c8a71c2109e
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-09-24 17:38:04 +0800

    resolves #36

--- R/plot_setting_matrices.R
+++ R/plot_setting_matrices.R
@@ -12,14 +12,18 @@
 #' @export
 plot_setting_matrices <- function(matrices,
                                   title = "Setting-specific synthetic contact matrices") {
-  plot_matrix(matrices$home) +
-    ggplot2::ggtitle("home") +
-    plot_matrix(matrices$school) +
-    ggplot2::ggtitle("school") +
-    plot_matrix(matrices$work) +
-    ggplot2::ggtitle("work") +
-    plot_matrix(matrices$other) +
-    ggplot2::ggtitle("other") +
+  
+  plot_home <- plot_matrix(matrices$home) + ggplot2::ggtitle("home")
+  plot_school <- plot_matrix(matrices$school) + ggplot2::ggtitle("school") 
+  plot_work <- plot_matrix(matrices$work) + ggplot2::ggtitle("work")
+  plot_other <- plot_matrix(matrices$other) + ggplot2::ggtitle("other")
+    
+  patchwork::wrap_plots(
+    plot_home,
+    plot_school,
+    plot_work,
+    plot_other
+  ) +
     patchwork::plot_layout(ncol = 2) +
     patchwork::plot_annotation(
       title = title
