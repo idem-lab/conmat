f96834d285ebd3cd60cd6f08ffdb163f7d14892c
Nick Golding <goldingn35@gmail.com>
2021-10-10 11:11:12 +0800

    get tests working for new get_age_population_function()

--- R/get_age_population_function.R
+++ R/get_age_population_function.R
@@ -18,10 +18,10 @@ get_age_population_function <- function(population) {
   
   # prepare population data for modelling
   pop_model <- population %>%
-    arrange(
+    dplyr::arrange(
       lower.age.limit
     ) %>%
-    mutate(
+    dplyr::mutate(
       # model based on bin midpoint
       bin_width = bin_widths(lower.age.limit),
       midpoint = lower.age.limit + bin_width / 2,
@@ -35,7 +35,7 @@ get_age_population_function <- function(population) {
   
   # filter to just the bounded age groups for fitting
   pop_model_bounded <- pop_model %>%
-    filter(
+    dplyr::filter(
       lower.age.limit < max_bound
     )
   
@@ -49,44 +49,44 @@ get_age_population_function <- function(population) {
       smooth.spline(
         x = midpoint,
         y = log_pop,
-        df = 10
+        df = pmin(10, nrow(pop_model_bounded))
       )
     )
   
   # predict to a long range of ages, to deal with upper bound
-  pred <- tibble(
+  pred <- tibble::tibble(
     age = 0:200
   ) %>%
-    mutate(
+    dplyr::mutate(
       log_pred = predict(fit, age)$y,
       pred = exp(log_pred)
     ) %>%
     # group into whether it is in the bounded or unbounded population
-    mutate(
+    dplyr::mutate(
       bounded = age < max_bound,
     ) %>%
-    group_by(
+    dplyr::group_by(
       bounded
     ) %>%
     # adjust populations within bounded ages to match totals
-    mutate(
-      required_pop = if_else(bounded, bounded_pop, unbounded_pop),
+    dplyr::mutate(
+      required_pop = ifelse(bounded, bounded_pop, unbounded_pop),
       modelled_pop = sum(pred),
       ratio = required_pop / modelled_pop,
       pred_adj = pred * ratio
     ) %>%
-    ungroup() %>%
+    dplyr::ungroup() %>%
     # adjust the unbounded region to drop off smoothly, based on the weights
-    mutate(
+    dplyr::mutate(
       # this is a weird way of getting the population of the final age bin in
       # the bounded group. Needs to happen after the previous grouped
       # reweighting step, and needs to be ungrouped now to do it.
       max_bound_pop = pred_adj[bounded][sum(bounded)],
     ) %>%
-    group_by(
+    dplyr::group_by(
       bounded
     ) %>%
-    mutate(
+    dplyr::mutate(
       # linearly extrapolate the final population group over years past the
       # upper bound. Select the number of years past such that all the excess
       # population is used up
@@ -98,29 +98,29 @@ get_age_population_function <- function(population) {
       weight = weight * target_weight_sum / weight_sum,
       population = ifelse(bounded, pred_adj, max_bound_pop * weight)
     ) %>%
-    ungroup() %>%
-    select(
+    dplyr::ungroup() %>%
+    dplyr::select(
       age,
       population
     ) %>%
-    filter(
+    dplyr::filter(
       population > 0
     )
   
   # return a function to look up populations for integer ages
   function(age) {
     
-    tibble(
+    tibble::tibble(
       age = age
     ) %>%
-      left_join(
+      dplyr::left_join(
         pred,
         by = "age"
       ) %>%
-      mutate(
-        population = replace_na(population, 0)
+      dplyr::mutate(
+        population = tidyr::replace_na(population, 0)
       ) %>%
-      pull(
+      dplyr::pull(
         population
       )
     
