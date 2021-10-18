26b70648a4c4d7a9efcdfefb88c6d5fadf6c7277
Nick Golding <goldingn35@gmail.com>
2021-10-18 22:57:59 +0800

    update setting transmission parameters again

--- R/get_setting_transmission_matrices.R
+++ R/get_setting_transmission_matrices.R
@@ -1,22 +1,44 @@
 #' Get Setting Transmission Matrices
 #'
-#' Given some age breaks, return a named list of matrices containing relative
-#' per-contact transmission probability matrices for each of 4 settings: home,
-#' school, work, other. These can be combined with contact matrices to produce
-#' setting-specific relative next generation matrices (NGMs). These can be
-#' scaled to match a required reproduction number based on the dominant
-#' eigenvalue of the all-settings NGM (the elementwise sum of all setting NGMs)
+#' Given some age breaks, return a named list of matrices containing
+#' age-specific relative per-contact transmission probability matrices for each
+#' of 4 settings: home, school, work, other. These can be combined with contact
+#' matrices to produce setting-specific relative next generation matrices
+#' (NGMs). These can be scaled to match a required reproduction number based on
+#' the dominant eigenvalue of the all-settings NGM (the elementwise sum of all
+#' setting NGMs).
+#'
+#' These matrices are created from: an estimate of the clinical fraction for
+#' each age (inferred by applying a smoothing spline to the mean estimates from
+#' Davies et al.); an assumption of the infectiousness of asymptomatics relative
+#' to symptomatics (provided as an argument); estimates of the relative
+#' susceptibility to infection of individuals of different ages, inferred from a
+#' smoothing-spline estimate of the mean relative susceptibility estimate from
+#' Davies et al., combined with a re-estimation of the susceptibility profile
+#' for under-16s, estimated in a similar way but to the age-distribution of
+#' infections in England from the UK ONS prevalence survey (rather than case
+#' counts with may undercount children), assuming the above clinical fraction
+#' estimates, and accounting for vaccination, reduced mixing, and reduced
+#' transmissibility in work and other settings due to hygiene behaviour; and
+#' estimates of the relative transmissibility in household vs non-household
+#' settings - scaled linearly for non-household transmission and binomially for
+#' household transmission (so that onward onfections do not to exceed the number
+#' of other household members).
 #'
 #' When using this data, ensure that you cite this package, and the original
-#' authors of the paper from which these estimates were derived at:
+#' authors of the paper from which these estimates were derived:
 #'
-#' Eyre, D. W., Taylor, D., Purver, M., Chapman, D., Fowler, T., Pouwels, K. B.,
-#' Walker, A. S., & Peto, T. E. (2021). The impact of SARS-CoV-2 vaccination on
-#' Alpha & Delta variant transmission (Preprint). medRxiv 2021.09.28.21264260
-#' https://doi.org/10.1101/2021.09.28.21264260
+#' Davies, N.G., Klepac, P., Liu, Y. et al. Age-dependent effects in the
+#' transmission and control of COVID-19 epidemics. Nat Med 26, 1205–1211 (2020).
+#' https://doi.org/10.1038/s41591-020-0962-9
 #'
 #' @param age_breaks vector of age breaks, defaults to `c(seq(0, 80, by = 5),
 #'   Inf)`
+#' @param asymptomatic_relative_infectiousness the assumed ratio of onward
+#'   infectiousness between asymptomatic and symptomatic cases
+#' @param susceptibility_estimate which estimate to use for susceptibiloty by
+#'   age - the smoothed original Davies et al estimates, or the set updated to match UK
+#'   under-16 infections (the default)
 #'
 #' @return list of matrices, containing the relative per-contact transmission
 #'   probability for each setting
@@ -64,75 +86,126 @@
 #' ngm_overall <- Reduce("+", next_generation_matrices)
 #' }
 get_setting_transmission_matrices <- function(
-  age_breaks = c(seq(0, 80, by = 5), Inf)
+  age_breaks = c(seq(0, 80, by = 5), Inf),
+  asymptomatic_relative_infectiousness = 0.5,
+  susceptibility_estimate = c("davies_updated", "davies_original")
   ) {
+
+  # which parameter estimates to use for susceptibility by age 
+  susceptibility_estimate <- rlang::arg_match(susceptibility_estimate)
   
-    # format the setting transmission scalings (calibrated for thesetransmission
-    # probabilities and conmat contact matrices against English infection data)
-    # into a tibble to join to the transmission probabilities
-    setting_weights_tibble <- tibble::tibble(setting = names(setting_weights),
-                                             weight = setting_weights)
-    
-    # aggregate the transmission probabilities according to the age breaks
-    eyre_transmission_probabilities %>%
-      dplyr::select(setting,
-                    age_to = contact_age,
-                    age_from = case_age,
-                    probability) %>%
-      dplyr::mutate(
-        age_group_to = cut(pmax(0.1, age_to), age_breaks, right = FALSE),
-        age_group_from = cut(pmax(0.1, age_from), age_breaks, right = FALSE),
-      )  %>%
-      dplyr::group_by(setting,
-                      age_group_from,
-                      age_group_to) %>%
-      dplyr::summarise(across(probability,
-                              mean),
-                       .groups =  "drop") %>%
-      # convert into matrices
-      tidyr::nest(matrix = c(age_group_from, age_group_to, probability)) %>%
-      dplyr::mutate(
-        matrix = lapply(
-          matrix,
-          tidyr::pivot_wider,
-          names_from = age_group_to,
-          values_from = probability
-        ),
-        matrix = lapply(matrix,
-                        tibble::column_to_rownames,
-                        "age_group_from"),
-        matrix = lapply(matrix,
-                        as.matrix)
-      ) %>%
-      # pivot, to relabel the ones we want as per the contact settings, then pivot
-      # back to long format
-      tidyr::pivot_wider(names_from = setting,
-                         values_from = matrix) %>%
+  # format the setting transmission scalings (calibrated for these transmission
+  # probabilities and conmat contact matrices against English infection data)
+  # into a tibble to join to the transmission probabilities
+  setting_weights_tibble <- tibble::tibble(
+    setting = names(setting_weights),
+    weight = setting_weights
+  )
+  
+  # load the age-dependent susceptibility and clinical fraction parameters, and
+  # convert into infectiousness and susceptibility
+  age_effects <- davies_age_extended %>%
+    dplyr::mutate(
+      infectiousness = clinical_fraction + (1 - clinical_fraction) *
+        asymptomatic_relative_infectiousness
+    ) %>%
+    # select which set to use
+    dplyr::select(
+      age,
+      infectiousness,
+      susceptibility = !!susceptibility_estimate
+    )
+  
+  # expand out to all settings and age combinations
+  data <- tidyr::expand_grid(
+    setting = setting_weights_tibble$setting,
+    age_from = age_effects$age,
+    age_to = age_effects$age
+  ) %>%
+    dplyr::left_join(
       dplyr::select(
-        home = household,
-        school = work_education,
-        work = work_education,
-        other = work_education
-      ) %>%
-      tidyr::pivot_longer(cols = everything(),
-                          names_to = "setting",
-                          values_to = "matrix") %>%
-      # add on the setting weights and multiply to reweight the relative probabilities
-      dplyr::left_join(setting_weights_tibble,
-                       by = "setting") %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(
-        matrix = dplyr::case_when(
-          setting == "home" ~ list(1 - (1 - matrix) ^ weight),
-          TRUE ~ list(matrix * weight)
-        )
-      ) %>%
-      # turn this into a list to return
-      dplyr::select(-weight) %>%
-      tidyr::pivot_wider(names_from = setting,
-                         values_from = matrix) %>%
-      as.list() %>%
-      lapply(purrr::pluck,
-             1)
+        age_effects,
+        age_from = age,
+        infectiousness
+        ),
+      by = "age_from"
+    ) %>%
+    dplyr::left_join(
+      dplyr::select(
+        age_effects,
+        age_to = age,
+        susceptibility
+      ),
+      by = "age_to"
+    ) %>%
+    # aggregate them to the required age ranges
+    dplyr::mutate(
+      age_group_to = cut(pmax(0.1, age_to), age_breaks, right = FALSE),
+      age_group_from = cut(pmax(0.1, age_from), age_breaks, right = FALSE),
+      .after = setting
+    ) %>%
+    dplyr::group_by(
+      setting,
+      age_group_from,
+      age_group_to
+    ) %>%
+    dplyr::summarise(
+      across(
+        c(infectiousness, susceptibility),
+        mean
+      ),
+      .groups =  "drop"
+    ) %>%
+    # attach and apply the weights
+    dplyr::left_join(
+      setting_weights_tibble,
+      by = "setting"
+    ) %>%
+    dplyr::mutate(
+      relative_probability = infectiousness * susceptibility,
+      probability = dplyr::case_when(
+        setting == "home" ~ 1 - (1 - relative_probability) ^ weight,
+        TRUE ~ relative_probability * weight
+      )
+    ) %>%
+    dplyr::select(
+      setting,
+      age_group_from,
+      age_group_to,
+      probability
+    )
     
-  }
+  matrices <- data %>%
+    # convert into matrices
+    tidyr::nest(
+      matrix = c(
+        age_group_from,
+        age_group_to,
+        probability
+      )
+    ) %>%
+    dplyr::mutate(
+      matrix = lapply(
+        matrix,
+        tidyr::pivot_wider,
+        names_from = age_group_to,
+        values_from = probability
+      ),
+      matrix = lapply(matrix,
+                      tibble::column_to_rownames,
+                      "age_group_from"),
+      matrix = lapply(matrix,
+                        as.matrix)
+    ) %>%
+    # turn this into a list to return
+    tidyr::pivot_wider(
+      names_from = setting,
+      values_from = matrix
+    ) %>%
+    as.list() %>%
+    lapply(purrr::pluck, 1)
+  
+  matrices[c("home", "school", "work", "other")]
+  
+}
+
--- data-raw/read_eyre_setting_weights.R
+++ data-raw/read_eyre_setting_weights.R
@@ -1,6 +1,6 @@
-setting_weights <- c(home = 0.2395682202164,
-                     school = 0.3374276064163,
-                     work = 0.3374276064163, 
-                     other = 0.3374276064163)
+setting_weights <- c(home = 0.543092149211357,
+                     school = 0.765559301800594,
+                     work = 0.765559301800594, 
+                     other = 0.765559301800594)
 
-use_data(setting_weights)
+use_data(setting_weights, overwrite = TRUE)
