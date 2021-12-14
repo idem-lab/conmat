e0af6f22af5809c5a43d39922e8b9e734adc0c67
aarathybabu97 <abab0012@student.monash.edu>
2021-12-14 16:19:20 +1100

    documentation for polymod_settings_models data export

--- R/data_polymod_model.R
+++ R/data_polymod_model.R
@@ -1,12 +1,21 @@
-#' Polymod Settings model
+#' Polymod Settings models
 #' 
-#' Add details on how the model was fit using the following code (copy in the 
-#'  model fitting code from "create-polymod-model.R"):
-#'  
-#' ```
+#' A data object containing a list of fitted gam models predicting the number of contacts in each of the four 
+#'   settings which are "home","work","school" and "other". For more details on model fitting, see [fit_setting_contacts()]
 #' 
-#' ``` 
+#' @seealso [fit_setting_contacts()]
+#' @examples 
+#' \dontrun{
+#' library(conmat)
+#' polymod_contact_data <- get_polymod_setting_data()
+#' polymod_survey_data <- get_polymod_population()
+#'polymod_setting_models <- fit_setting_contacts(
+#'  contact_data_list = polymod_contact_data,
+# population = polymod_survey_data
+#')
+#' }
+#' 
+#' 
 #'
-#' @format A model predicting the number of contacts in each of the four 
-#'   settings, which are: ..., ...
+#' 
 "polymod_setting_models"
