19c1b437f36661eea8dad28a0c60602fbbe13fa2
Aarathy <aarathybabu907@gmail.com>
2022-04-01 15:35:40 +1100

    apply_vaccination function

--- R/apply_vaccination.R
+++ R/apply_vaccination.R
@@ -1,64 +1,61 @@
-ngm_NSW <- generate_ngm(
-  state_name = "NSW", 
-  age_breaks = c(seq(0, 80, by = 5), Inf),
-  R_target = 1.5
-) 
-
-# ideal function 
-# - takes in matrix ngm, with ordered vectors of coverage, acquisition and transmission efficacies
-ngm_5y_fairfield_vacc_0.5 <- apply_vaccination(
-  ngm = ngm_5y_fairfield,
-  coverage_by_age = age_coverage,
-  efficacy_acquistion = efficacy_acquisition,
-  efficacy_transmission = efficacy_transmission
-)
-
-library(tidyverse)
-
-ves <- read_csv("data/example_ves.csv", col_types = list(col_factor(), col_double(), col_double()))
-coverage <- read_csv("data/example_coverage.csv", col_types = list(col_factor(), col_double()))
-
-coverage
-ves
-
-ngm_NSW$all
+#' Apply vaccination effects to next generation contact matrices
+#' @param ngm next generation matrices. See [generate_ngm()] for creating next generation matrices of a state or a local government area for specific age groups
+#' @param data data set with location specific information on vaccine coverage, efficacy of acquisition/susceptibility and efficacy of transmission/infectiousness for the ordered age groups from lowest to highest of the next generation matrix 
+#' @param coverage_col bare variable name for the column with information on vaccine coverage by age groups
+#' @param acquisition_col bare variable name for the column with information on efficacy of acquisition
+#' @param transmission_col bare variable name for the column with information on efficacy of transmission
+#' @export
+#' @examples
+#' 
+#' vaccination_effect_example_data
+#' ngm_NSW <- generate_ngm(state_name = "NSW", 
+#'                         age_breaks = c(seq(0, 80, by = 5),Inf),
+#'                         R_target = 1.5
+#'                         ) 
+#' ngm_5y_NSW_vacc_0.5 <- apply_vaccination(ngm = ngm_NSW,
+#'                                          data=vaccination_effect_example_data,
+#'                                          coverage_col=coverage,
+#'                                          acquisition_col=acquisition,
+#'                                          transmission_col=transmission)
+#' 
 
 
-ves %>%
-  # join on coverages
-  left_join(
-    coverage,
-    by = c("age_band")
-  ) %>%
+apply_vaccination <- function(ngm,
+                              data,
+                              coverage_col,
+                              acquisition_col,
+                              transmission_col){
+
+  
+  transmission_reduction_matrix <- data %>%
   # compute percentage reduction in acquisition and transmission in each age group
-  mutate(
-    acquisition_multiplier = 1 - acquisition * coverage,
-    transmission_multiplier = 1 - transmission * coverage,
+  dplyr::mutate(
+    acquisition_multiplier = 1 - {{ acquisition_col }} * {{ coverage_col }},
+    transmission_multiplier = 1 - {{ transmission_col }} * {{ coverage_col }},
   ) %>%
-  select(
-    -acquisition,
-    -transmission,
-    -coverage
-  ) %>%
-  # transform these into matrices of reduction in transmission, matching the NGM
-  summarise(
-    transmission_reduction_matrix =
-      list(
-        outer(
-          # 'to' groups are on the rows in conmat, and first element in outer is rows,
-          # so acquisition first
-          acquisition_multiplier,
-          transmission_multiplier,
-          FUN = "*"
-        )
-      ),
-    .groups = "drop"
-  ) %>%
-  mutate(
-    ngm_unvaccinated = list(ngm_NSW$all),
-    ngm_vaccinated = list(ngm_unvaccinated[[1]] * transmission_reduction_matrix[[1]])
-  ) %>%
-  pull(ngm_vaccinated) %>%
-  `[[`(1)
+  dplyr::select(-c(
+    {{ coverage_col }},
+    {{ acquisition_col }},
+    {{ transmission_col }})
+  )%>%
+    # transform these into matrices of reduction in transmission, matching the NGM
+    summarise(
+      transmission_reduction_matrix =
+        list(
+          outer(
+            # 'to' groups are on the rows in conmat, and first element in outer is rows,
+            # so acquisition first
+            acquisition_multiplier,
+            transmission_multiplier,
+            FUN = "*"
+          )
+        ),
+      .groups = "drop"
+    ) %>%
+   dplyr::pull(transmission_reduction_matrix)
+  
+ngm_vaccinated=Map('*',ngm,transmission_reduction_matrix)
+return(ngm_vaccinated)
 
-# Aarathy to tidy this as she sees fit.
+}
+
