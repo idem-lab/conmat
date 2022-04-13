aabf72c4567b0ed67ebb7f9e89bf790afc34377f
Aarathy <aarathybabu907@gmail.com>
2022-04-13 13:23:29 +1000

    documentation fixes as per review

--- R/data_example_vaccination_effect.R
+++ R/data_example_vaccination_effect.R
@@ -1,7 +1,7 @@
-#' Example data set with information on age based vaccination coverage,acquisition and transmission
+#' Example dataset with information on age based vaccination coverage,acquisition and transmission
 #'
-#' data set with information on vaccine coverage, efficacy of acquisition/susceptibility and efficacy of transmission/infectiousness 
-#' for the ordered age groupsfrom lowest to highest of the next generation matrix 
+#' data frame with information on vaccine coverage, efficacy of acquisition/susceptibility and efficacy of transmission/infectiousness 
+#' for the ordered age groups from lowest to highest of the next generation matrix 
 #'
 #' @format Tibble
 "vaccination_effect_example_data"
