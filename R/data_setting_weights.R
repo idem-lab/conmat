ffc6f459a0cdfbabca12fc88d810d000ebb1e7c8
Aarathy <aarathybabu907@gmail.com>
2022-04-29 18:44:24 +1000

    renamed filename and removed out eyre citation

---
+++ R/data_setting_weights.R
@@ -0,0 +1,8 @@
+#' Setting weights computed for transmission probabilities.
+#'
+#'
+#' see ?get_setting_transmission_matrices for details of how to use these
+#'
+#' @format A named vector of weights relative to home, for home, work, school,
+#'   and other
+"setting_weights"
---
+++ data-raw/read_setting_weights.R
@@ -0,0 +1,6 @@
+setting_weights <- c(home = 0.732154285228522,
+                     school = 0.286882796193768,
+                     work = 0.286882796193768, 
+                     other = 0.286882796193768)
+
+use_data(setting_weights, overwrite = TRUE)
