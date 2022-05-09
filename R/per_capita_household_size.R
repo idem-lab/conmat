8ee71471c1c1e44d9874f18521084d646c6db68a
Aarathy <aarathybabu907@gmail.com>
2022-05-09 18:19:41 +1000

    split get_per_capita_household_size into 2 functions

---
+++ R/per_capita_household_size.R
@@ -0,0 +1,42 @@
+#' @title Get per capita household size with household size distribution
+#' @description Returns the per capita household size for a location given its household size distribution. 
+#' See [get_household_size_distribution()] function for retrieving household size distributions for a given place.
+#' @param household_data data set with information on the household size distribution of specific state or LGA. 
+#' @param household_size bare variable name of the column depicting the household size. Default is 'household_size' from [get_household_size_distribution()].
+#' @param n_people bare variable name of the column depicting the total number of people belonging to the respective household size. Default is 'n_people' from [get_household_size_distribution()]. 
+#' @return Numeric of length 1 - the per capita household size for a given state
+#'     or LGA.
+#' @author Nick Golding
+#' @export
+#' @examples 
+#' sample_data <- get_household_size_distribution(lga = "Fairfield (C)")
+#' per_capita_household_size(sample_data,household_size,n_people)
+#' 
+#' 
+per_capita_household_size <- function(household_data=sample_data,
+                                      household_size=household_size,
+                                      n_people=n_people) {
+ 
+  # aggregate and average household sizes
+  household_data %>%
+    dplyr::group_by(
+      {{ household_size }},
+      .add = TRUE
+    ) %>%
+    dplyr::summarise(
+      n_people = sum({{ n_people }}),
+      .groups = "drop"
+    ) %>%
+    dplyr::mutate(
+      # as a fraction of the population
+      fraction = n_people / sum(n_people)
+    ) %>%
+    dplyr::summarise(
+      per_capita_household_size = sum({{ household_size }} * fraction),
+      .groups = "drop"
+    ) %>%
+    dplyr::pull(
+      per_capita_household_size
+    )
+  
+}
