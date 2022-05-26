d92e1973fbc39cba7b98d5f0621e8a609bc61a88
Aarathy <aarathybabu907@gmail.com>
2022-05-26 11:17:51 +1000

    state & lga wise data for education and work

---
+++ R/get_data_abs_age_education.R
@@ -0,0 +1,28 @@
+#' @title Return educated population data for a given age and state or lga of Australia
+#' @param state target Australian state name in abbreviated form, such as "QLD", "NSW", or "TAS"
+#' @param lga target Australian local government area (LGA) name, such as "Fairfield (C)".  See
+#'   [abs_lga_lookup()] for list of lga names
+#' @param age a numeric denoting age
+#' @return dataset with information on the educated population belonging to the given age, its total population
+#'  and the corresponding proportion.
+#' @export
+#' @examples
+#' get_data_abs_age_education(state="NSW",age=9)
+#' get_data_abs_age_education(lga="Albury (C)",age=10)
+get_data_abs_age_education <- function(age, state=NULL, lga=NULL) {
+  
+  if (!is.null(state)) {
+    check_state_name(state)
+    
+    data <- data_abs_state_education %>%
+      dplyr::filter(state=={{ state }})
+    
+  } else{
+    check_lga_name(lga)
+    data <- data_abs_lga_education %>%
+      dplyr::filter(lga=={{ lga }})
+  }
+  # check LGA name
+  data %>%
+    filter(age == {{ age }})
+}
