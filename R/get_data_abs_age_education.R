b0029e05876b6c739d7ce2c6d93c6d83e8a6945a
Aarathy <aarathybabu907@gmail.com>
2022-05-26 11:46:29 +1000

    age variable made to accept numeric vector + dplyr added + documentation edits

--- R/get_data_abs_age_education.R
+++ R/get_data_abs_age_education.R
@@ -1,28 +1,28 @@
-#' @title Return educated population data for a given age and state or lga of Australia
+#' @title Return  data on educated population for a given age and state or lga of Australia
 #' @param state target Australian state name in abbreviated form, such as "QLD", "NSW", or "TAS"
 #' @param lga target Australian local government area (LGA) name, such as "Fairfield (C)".  See
 #'   [abs_lga_lookup()] for list of lga names
-#' @param age a numeric denoting age
-#' @return dataset with information on the educated population belonging to the given age, its total population
+#' @param age a numeric or numeric vector denoting ages between 0 to 115
+#' @return dataset with information on the number of educated people belonging to a particular age, its total population
 #'  and the corresponding proportion.
 #' @export
 #' @examples
-#' get_data_abs_age_education(state="NSW",age=9)
+#' get_data_abs_age_education(state="VIC",age=1:10)
 #' get_data_abs_age_education(lga="Albury (C)",age=10)
 get_data_abs_age_education <- function(age, state=NULL, lga=NULL) {
   
   if (!is.null(state)) {
     check_state_name(state)
     
-    data <- data_abs_state_education %>%
-      dplyr::filter(state=={{ state }})
+    data_subset <- data_abs_state_education %>%
+      dplyr::filter(state %in% {{ state }})
     
   } else{
     check_lga_name(lga)
-    data <- data_abs_lga_education %>%
-      dplyr::filter(lga=={{ lga }})
+    data_subset <- data_abs_lga_education %>%
+      dplyr::filter(lga %in% {{ lga }})
   }
-  # check LGA name
-  data %>%
-    filter(age == {{ age }})
+ 
+  data_subset %>%
+    dplyr::filter(age %in% {{ age }})
 }
