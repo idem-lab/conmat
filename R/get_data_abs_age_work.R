4731c7abf74712e4d487fea44feb5fbed4b94608
Aarathy <aarathybabu907@gmail.com>
2022-05-30 17:24:28 +1000

    changes mentioned in PR #66 added

--- R/get_data_abs_age_work.R
+++ R/get_data_abs_age_work.R
@@ -7,22 +7,63 @@
 #'  and the corresponding proportion.
 #' @export
 #' @examples
-#' get_data_abs_age_work(state="NSW",age=10:20)
-#' get_data_abs_age_work(lga="Barcoo (S)",age=39)
-get_data_abs_age_work <- function(age, state=NULL, lga=NULL) {
-  
-  if (!is.null(state)) {
-    check_state_name(state)
+#' get_data_abs_age_work(state="NSW")
+#' get_data_abs_age_work(state="WA",lga="Albany (C)",age=1:5)
+#' get_data_abs_age_work(lga="West Tamar (M)",state="SA",age=1:5)
+#' get_data_abs_age_work(state=c("QLD","TAS"),age=5)
+#' get_data_abs_age_work(lga=c("Albury (C)","Barcoo (S)"),age=39)
+
+get_data_abs_age_work <- function(age=NULL, state=NULL, lga=NULL) {
+  # checks only for state 
+  if (!is.null(state) & is.null(lga)) {
     
-    data <- data_abs_state_work %>%
+    check_state_name(state)
+    data_subset <- data_abs_state_work %>%
       dplyr::filter(state %in% {{ state }})
     
   } else{
-    check_lga_name(lga)
-    data <- data_abs_lga_work %>%
-      dplyr::filter(lga %in% {{ lga }})
+    # checks for lga 
+    if (!is.null(lga)) {
+      
+      check_lga_name(lga, multiple_lga = TRUE)
+      
+      # checks if there is a state argument as well
+      # if state argument present, it filters for it
+      if (!is.null(state))
+      {
+        data_subset <- data_abs_lga_work %>%
+          dplyr::filter(lga %in% {{ lga }} , state %in% {{ state }})
+        
+        # if empty tibble , error message 
+        if (nrow(data_subset) == 0)
+        {
+          rlang::abort(
+            message = c(
+              "The LGA name provided does not belong to the state",
+              i = "Specify the exact LGA name and the corresponding state it belongs to. See `abs_lga_lookup` for a list of all LGAs and the state it belongs to",
+              x = glue::glue(
+                "The lga name '{lga}' does not belong to the state '{state}'"
+              )
+            )
+          )
+        } else{
+          # if tibble not empty, do nothing
+          data_subset 
+        }
+      } else {
+        # if there is no state argument along with lga
+        data_subset <- data_abs_lga_work %>%
+          dplyr::filter(lga %in% {{ lga }})
+      }
+    }
+  } # end check for state & lga
+  if (!is.null(age))
+  {
+    data_subset %>%
+      dplyr::filter(age %in% {{ age }})
+    
+  } else {
+    data_subset
   }
-  
-  data %>%
-    dplyr::filter(age %in% {{ age }})
-}
+
+  }
