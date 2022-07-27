4d957e8162065449a891489981f4ee21c2163996
Aarathy <aarathybabu907@gmail.com>
2022-07-27 12:18:53 +1000

    comments on modelling features

--- R/add_modelling_features.R
+++ R/add_modelling_features.R
@@ -12,17 +12,17 @@
 #' @note internal
 #' @export
 add_modelling_features <- function(contact_data, ...) {
-  contact_data %>%
-   
-    # use interpolated population of "age_to" (contact age) & 
-    # get the relative population grouped by "age_from" or participant age
+  
+  # use interpolated population of "age_to" (contact age) & 
+  # get the relative population grouped by "age_from" or participant age
+  # add new variables for : school & work going fraction for contact & participant ages
+  #                       : probability that a person of the other age goes to the same  work/school 
+  #                       : probability that a person of the other age would be in the same school year
+  #                       : weighted combination of contact population & school year probability. 
+  #                       [ for using outside of classroom?]
+  # offset for school setting & the rest. 
+   contact_data %>%
     add_population_age_to(...) %>%
-    # add new variables for : school & work going fraction for contact & participant ages
-    #                       : probability that a person of the other age goes to the same  work/school 
-    #                       : probability that a person of the other age would be in the same school year
-    #                       : weighted combination of contact population & school year probability. 
-    #                       [ for using outside of classroom?]
     add_school_work_participation() %>%
-    # offset for school setting & the rest. 
     add_offset()
 }
