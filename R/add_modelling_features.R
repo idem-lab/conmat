5e692f345b0c448d5624ce8676e4d88c794458f8
njtierney <nicholas.tierney@gmail.com>
2022-08-09 11:52:51 +0800

    first pass on symmetrical features

--- R/add_modelling_features.R
+++ R/add_modelling_features.R
@@ -16,6 +16,7 @@ add_modelling_features <- function(contact_data, ...) {
     # Adds interpolated age population - specifically, `pop_age_to`
     add_population_age_to(...) %>%
     # Adds school and work offset
+    add_symmetrical_features() %>%
     add_school_work_participation() %>%
     # adds columns 
     # `log_contactable_population_school`, and ` log_contactable_population`
