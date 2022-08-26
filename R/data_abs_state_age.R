5ce65fac2c4c59d5fd2567745ae76b8a4765f966
njtierney <nicholas.tierney@gmail.com>
2022-08-26 16:17:49 +0800

    documentation and code makeover

--- R/data_abs_state_age.R
+++ R/data_abs_state_age.R
@@ -1,6 +1,7 @@
 #' ABS state population data for 2020
 #'
-#' A dataset containing Australian Bureau of Statistics state level population data for 2020
+#' Dataset containing Australian Bureau of Statistics state level population 
+#'   data for 2020
 #'
 #' @format A data frame with 168 rows and 3 variables:
 #' \describe{
--- R/get_polymod_contact_data.R
+++ R/get_polymod_contact_data.R
@@ -13,11 +13,21 @@
 #' }
 #' }
 #' @export
-get_polymod_contact_data <- function(setting = c("all", "home", "work", "school", "other"),
-                                     countries = c("Belgium", "Finland", "Germany", "Italy", "Luxembourg", "Netherlands", 
-                                                 "Poland", "United Kingdom"),
-                                     ages = 0:100,
-                                     contact_age_imputation = c("sample", "mean", "remove_participant")) {
+get_polymod_contact_data <- function(
+  setting = c("all", "home", "work", "school", "other"),
+  countries = c(
+    "Belgium",
+    "Finland",
+    "Germany",
+    "Italy",
+    "Luxembourg",
+    "Netherlands",
+    "Poland",
+    "United Kingdom"
+  ),
+  ages = 0:100,
+  contact_age_imputation = c("sample", "mean", "remove_participant")
+) {
   setting <- match.arg(setting)
   contact_age_imputation <- match.arg(contact_age_imputation)
 
