a6bc340ea331b86c2126593a959df0fa109d6b3d
njtierney <nicholas.tierney@gmail.com>
2022-09-15 17:50:47 +1200

    skip these tests on CI...

--- tests/testthat/test-get-polymod-population.R
+++ tests/testthat/test-get-polymod-population.R
@@ -1,14 +1,17 @@
 test_that("get_polymod_contact_data() works", {
+  skip_on_ci()
   set.seed(2021-10-4)
   expect_snapshot(get_polymod_contact_data())
 })
 
 test_that("get_polymod_population() works", {
+  skip_on_ci()
   set.seed(2021-10-4)
   expect_snapshot(get_polymod_population())
 })
 
 test_that("get_polymod_setting_data() works", {
+  skip_on_ci()
   set.seed(2021-10-4)
   expect_snapshot(get_polymod_setting_data())
 })
