c9eb15e06bb216427f27872136167798821269d2
njtierney <nicholas.tierney@gmail.com>
2022-09-16 10:45:19 +1200

    skip on CI

--- tests/testthat/test-get-polymod-population.R
+++ tests/testthat/test-get-polymod-population.R
@@ -1,17 +1,17 @@
 test_that("get_polymod_contact_data() works", {
-  options(pillar.print_max = 15)
+  skip_on_ci()
   set.seed(2021-10-4)
   expect_snapshot_output(get_polymod_contact_data())
 })
 
 test_that("get_polymod_population() works", {
-  options(pillar.print_max = 15)
+  skip_on_ci()
   set.seed(2021-10-4)
   expect_snapshot_output(get_polymod_population())
 })
 
 test_that("get_polymod_setting_data() works", {
-  options(pillar.print_max = 15)
+  skip_on_ci()
   set.seed(2021-10-4)
   expect_snapshot_output(get_polymod_setting_data())
 })
