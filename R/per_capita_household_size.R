7cc152f0061806e3a563455d592b778451cf7b7f
Aarathy <aarathybabu907@gmail.com>
2022-10-13 19:25:17 +1100

    removed remnants of get_household_size_distribution()

--- R/per_capita_household_size.R
+++ R/per_capita_household_size.R
@@ -8,16 +8,16 @@
 #'   distribution of specific state or LGA. 
 #' @param household_size_col bare variable name of the column depicting the
 #'   household size. Default is 'household_size' from
-#'   [get_household_size_distribution()].
+#'   [abs_per_capita_household_size_lga()].
 #' @param n_people_col bare variable name of the column depicting the total
 #'   number of people belonging to the respective household size. Default is
-#'   'n_people' from [get_abs_household_size_distribution()]. 
+#'   'n_people' from [abs_per_capita_household_size_lga()]. 
 #' @return Numeric of length 1 - the per capita household size for a given 
 #'   state or LGA.
 #' @author Nick Golding
 #' @export
 #' @examples 
-#' demo_data <- get_abs_household_size_distribution(lga = "Fairfield (C)")
+#' demo_data <- abs_per_capita_household_size_lga(lga = "Fairfield (C)")
 #' demo_data
 #' per_capita_household_size(household_data=demo_data,
 #'                           household_size_col=household_size,
