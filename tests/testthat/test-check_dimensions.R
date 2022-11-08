c6609ea194a8ea2699b2b7866a9c3316a58d5f81
Aarathy <aarathybabu907@gmail.com>
2022-11-08 17:15:25 +1100

    demo matrix and data for tests on check_dimensions

--- tests/testthat/test-check_dimensions.R
+++ tests/testthat/test-check_dimensions.R
@@ -1,50 +1,48 @@
 
-ngm_fairfield_85_plus <- generate_ngm(
-  lga_name = "Fairfield (C)",
-  age_breaks = c(seq(0, 85, by = 5), Inf),
-  R_target = 1.5
-)
-
-ngm_fairfield_80_plus <- generate_ngm(
-  lga_name = "Fairfield (C)",
-  age_breaks = c(seq(0, 80, by = 5), Inf),
-  R_target = 1.5
-)
 
 
 
-test_that("check_dimensions() returns nothing when compatible dimensions", {
+test_that("check_dimensions() returns nothing when compatible dimensions",
+          {
+            list(matrix(1:6, nrow = 3, ncol = 2),
+                 matrix(1:6, nrow = 3, ncol = 2)) -> demo_matrix
+            
+            names(demo_matrix) <- c("matrix_a", "matrix_b")
+            
+            demo_data <- tibble::tibble(x = 1:2, y = 2 * x)
+            
+            expect_silent(check_dimensions(demo_matrix,
+                                           demo_data))
+          })
 
-  
 
-  
-  expect_silent(check_dimensions(ngm_fairfield_80_plus,
-                                         vaccination_effect_example_data))
-})
 
 test_that("check_dimensions() returns error", {
+  list(matrix(1:6, nrow = 3, ncol = 2),
+       matrix(1:6, nrow = 3, ncol = 2)) -> demo_matrix
   
-
-  expect_snapshot_error(check_dimensions(ngm_fairfield_85_plus,
-                                  vaccination_effect_example_data))
+  names(demo_matrix) <- c("matrix_a", "matrix_b")
+  
+  demo_data <- tibble::tibble(x = 1:6, y = 2 * x)
+  expect_snapshot_error(check_dimensions(demo_matrix,
+                                         demo_data))
 })
 
-test_that("apply_vaccination gives error when incompatible dimensions present", {
-
- expect_snapshot_error(apply_vaccination(
-    ngm = ngm_fairfield_85_plus,
-    data = vaccination_effect_example_data,
-    coverage_col = coverage,
-    acquisition_col = acquisition,
-    transmission_col = transmission
-  ))
-  
-})
-
-# ngm_nsw_vacc <- apply_vaccination(
-#   ngm = ngm_fairfield_80_plus,
-#   data = vaccination_effect_example_data,
-#   coverage_col = coverage,
-#   acquisition_col = acquisition,
-#   transmission_col = transmission
-# )
+test_that("apply_vaccination gives error when incompatible dimensions present",
+          {
+            ngm_fairfield_15_plus <- generate_ngm(
+              lga_name = "Fairfield (C)",
+              age_breaks = c(seq(0, 15, by = 5), Inf),
+              R_target = 1.5
+            )
+            expect_snapshot_error(
+              apply_vaccination(
+                ngm = ngm_fairfield_15_plus,
+                data = vaccination_effect_example_data,
+                coverage_col = coverage,
+                acquisition_col = acquisition,
+                transmission_col = transmission
+              )
+            )
+            
+          })
