078cba41e3468b902addb585d5024a77cf4924d8
njtierney <nicholas.tierney@gmail.com>
2022-12-19 15:44:55 +1000

    ABS functions now return a conmat population data frame

--- R/abs-helpers.R
+++ R/abs-helpers.R
@@ -9,9 +9,9 @@
 #' abs_age_lga(c("Albury (C)","Fairfield (C)"))
 #' abs_age_state(c("NSW","VIC"))
 abs_age_lga <- function(lga_name) {
-  check_lga_name(lga_name,multiple_lga = TRUE)
+  check_lga_name(lga_name, multiple_lga = TRUE)
 
-  abs_pop_age_lga_2020 %>%
+  abs_population <- abs_pop_age_lga_2020 %>%
     dplyr::filter(lga %in% lga_name) %>%
     dplyr::select(
       lga,
@@ -21,15 +21,22 @@ abs_age_lga <- function(lga_name) {
     ) %>%
     dplyr::mutate(age_group = readr::parse_number(as.character(age_group))) %>%
     dplyr::rename(lower.age.limit = age_group)
+
+  conmat_population(
+    data = abs_population,
+    age = lower.age.limit,
+    population = population
+  )
 }
 
 #' @param state_name shortened state name
 #' @rdname abs_age_data
 #' @export
 abs_age_state <- function(state_name) {
+  
   check_state_name(state_name,multiple_state = TRUE)
 
-  abs_pop_age_lga_2020 %>%
+  abs_population <- abs_pop_age_lga_2020 %>%
     dplyr::filter(state %in% state_name) %>%
     dplyr::select(
       state,
@@ -41,4 +48,10 @@ abs_age_state <- function(state_name) {
     dplyr::rename(lower.age.limit = age_group) %>%
     dplyr::group_by(year, state, lower.age.limit) %>%
     dplyr::summarise(population = sum(population))
+  
+  conmat_population(
+    data = abs_population,
+    age = lower.age.limit,
+    population = population
+  )
 }
