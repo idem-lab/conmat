8c36e745158b4ca12e76ad8d376518d61726edf6
Michael Lydeamore <michael.lydeamore@monash.edu>
2023-01-20 20:58:55 +1100

    add prem documentation

---
+++ R/data_prem_contact_matrices.R
@@ -0,0 +1,20 @@
+#' Contact matrices as calculated by Prem. et al.
+#'
+#' Contact matrices as calculated by Prem. et al. PLoS Computational Biology.
+#' DOI: 10.1371/journal.pcbi.1005697
+#'
+#' @format A list with 5 elements:
+#' \describe{
+#'   \item{home}{A 16x16 matrix containing the number of home contacts, by 5
+#'   year age group}
+#'   \item{work}{A 16x16 matrix containing the number of workplace contacts, by
+#'  5 year age group}
+#'   \item{school}{A 16x16 matrix containing the number of school contacts, by 5
+#'   year age group}
+#'   \item{other}{A 16x16 matrix containing the number of other contacts, by 5
+#'   year age group}
+#' }
+#' All age groups are 5 year age bands, from 0 to 80.
+#'
+#' @source \url{https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1005697}
+"prem_germany_contact_matrices"
