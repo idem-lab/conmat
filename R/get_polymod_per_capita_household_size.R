5d04bff93560a731ff6b75e68ec4045267a1e795
Michael Lydeamore <michael.lydeamore@monash.edu>
2024-07-26 16:18:52 +1000

    Fix typo in example

--- R/get_polymod_per_capita_household_size.R
+++ R/get_polymod_per_capita_household_size.R
@@ -6,7 +6,7 @@
 #' @return number, 3.248971
 #' @author Nicholas Tierney
 #' @examples
-#' get_polymod_per_capita_hosuehold_size()
+#' get_polymod_per_capita_household_size()
 #' @export
 get_polymod_per_capita_household_size <- function() {
   mean(socialmixr::polymod$participants$hh_size)
